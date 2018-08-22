e806b6778e40b16c460d8328cebfc6049cc279fc
Richard Tape <richard.tape@ubc.ca>
2018-08-21 18:08:11 -0700

    Added initial commit. Just the boilerplat main plugin file and the readme.

---
+++ wpprofiles-hub.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WP PRofiles Hub
+ *
+ * @package     WPPRofiles-Hub
+ * @author      Richard Tape, Jeremy Felt
+ * @copyright   2018 WPProfiles
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: WPPRofiles Hub
+ * Plugin URI:  https://wpprofiles.com/plugins/wpprofiles-hub/
+ * Description: The Hub for WP Profiles. Where folks control their profiles.
+ * Version:     0.0.1
+ * Author:      Richard Tape, Jeremy Felt
+ * Author URI:  https://wpprofiles.com/
+ * Text Domain: wpprrofiles-hub
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
