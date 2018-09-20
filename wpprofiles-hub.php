6835998617647b822d124a4c2d15b80ae8871e89
Richard Tape <richard.tape@ubc.ca>
2018-09-19 20:57:28 -0700

    Changed absolutely nothing as I really just want this damn slack integration to work and I hate everything.

--- wpprofiles-hub.php
+++ wpprofiles-hub.php
@@ -11,7 +11,7 @@
  * Plugin Name: WPProfiles Hub
  * Plugin URI:  https://wpprofiles.com/plugins/wpprofiles-hub/
  * Description: The Hub for WP Profiles. Where folks control their profiles.
- * Version:     0.0.6
+ * Version:     0.0.7
  * Author:      Richard Tape, Jeremy Felt
  * Author URI:  https://wpprofiles.com/
  * Text Domain: wpprrofiles-hub
