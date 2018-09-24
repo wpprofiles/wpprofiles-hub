3dbf165baf33604d4d58ac4c901bc2128d91d094
Richard Tape <richard.tape@ubc.ca>
2018-09-24 09:45:36 -0700

    Added instantiation of our user dashboard class as well as added a redirect for profile role users to send them to their user profile directly. This may change in the future and could be conditional on if the user has already 'completed' their profile. The profile user's dashboard could show notifications from sites asking to embed their profiles etc.

--- src/WPProfiles/Hub/class-wpprofiles.php
+++ src/WPProfiles/Hub/class-wpprofiles.php
@@ -21,6 +21,8 @@ class WPProfiles {
 
 		$this->register_hooks();
 
+		$this->setup_profile_user_dashboard();
+
 	}// end __construct()
 
 	/**
@@ -36,7 +38,21 @@ class WPProfiles {
 		// Run on plugin deactivation. Cleanup.
 		add_action( 'wp_profiles_hub_deactivate', array( $this, 'remove_roles' ) );
 
+		// For profile user role, redirect to user profile on log in
+		add_filter( 'login_redirect', array( $this, 'login_redirect__redirect_profile_user_to_profile' ), 20, 3 );
+
 	}// end register_hooks()
+
+	/**
+	 * Instantiate our class which handles the setup of the profile user screen (at /wp-admin/profile.php)
+	 *
+	 * @return void
+	 */
+	public function setup_profile_user_dashboard() {
+
+		$user_dash = new \WPProfiles\Hub\Dashboard\Profile_User();
+
+	}// end setup_profile_user_dashboard()
 
 	/**
 	 * Add our custom Role(s)
@@ -84,5 +100,24 @@ class WPProfiles {
 		$profile_role->deregister();
 
 	}// end remove_profile_role()
+
+	/**
+	 * When a user with the role of profile signs in, we redirect them to their
+	 * user profile screen, rather than their main WP Dashboard.
+	 *
+	 * @param string $redirect_to URL to redirect to.
+	 * @param string $request URL the user is coming from.
+	 * @param object $user Logged user's data.
+	 * @return string The potentially modified location to redirect to.
+	 */
+	public function login_redirect__redirect_profile_user_to_profile( $redirect_to, $request, $user ) {
+
+		if ( ! \WPProfiles\Hub\Utils::user_is_profile_user( $user ) ) {
+			return $redirect_to;
+		}
+
+		return admin_url( 'profile.php' );
+
+	}// end login_redirect__redirect_profile_user_to_profile()
 
 }// end class WPProfiles
